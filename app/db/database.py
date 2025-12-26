e6445a548dc3e8220854ab4dfe32f5039a3a7185
Selom Kwame Amuzu <selomka1@gmail.com>
2025-12-26 10:15:16 +0000

    Changed database config

--- app/db/database.py
+++ app/db/database.py
@@ -3,6 +3,12 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
-sessionLocal = sessionmaker(autoflush=False,bind=engine)
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=0
+)
+sessionLocal = sessionmaker(autoflush=False, bind=engine)
 Base = declarative_base()
